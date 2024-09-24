19ec37a4b2a953f611d5f1815c4ff9c5091089f2
HoangVanThong <hoanggvannthongg@gmail.com>
2024-09-24 21:37:21 +0700

    Complete Modal

--- accest/js/script.js
+++ accest/js/script.js
@@ -1,12 +1,29 @@
-// Form Đăng nhập
-var userElement = document.querySelector(".user");
-var modalElement = document.querySelector(".modal");
-var modalOverlayElement = document.querySelector(".modal__overlay");
-userElement.addEventListener("click", () => {
-  modalElement.classList.add("display");
-});
-window.onclick = function (event) {
-  if (event.target == modalOverlayElement) {
-    modalElement.classList.remove("display");
+// Modal Login
+const userElement = document.querySelector(".user");
+const cinemaElement = document.querySelector(".cinema");
+const modalLoginElement = document.querySelector(".modal__body-login");
+const modalCinemaElement = document.querySelector(".modal__body-cinema");
+const modalOverlayElement = document.querySelector(".modal__overlay");
+const modal = document.querySelector(".modal");
+const modals = document.querySelectorAll(".modal__body");
+
+const showModalContainer = function (displayModal) {
+  for (const modal of modals) {
+    modal.classList.remove("display");
   }
+  displayModal.classList.add("display");
+  modal.classList.add("display");
 };
+
+userElement.addEventListener("click", () => {
+  showModalContainer(modalLoginElement);
+});
+
+cinemaElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  showModalContainer(modalCinemaElement);
+});
+
+modalOverlayElement.addEventListener("click", () => {
+  modal.classList.remove("display");
+});
